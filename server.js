238b2b1d88e3d3ce6ba4f269db22f19635a2be21
musaomark01 <musaomark01@gmail.com>
2023-11-14 17:56:54 -0500

    debug and worked on server.js

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
